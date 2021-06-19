d5977af9add6365a9fc7e9593e982c58fee6ff5b
msaaddev <mrsaadirfan@gmail.com>
2021-06-20 02:32:41 +0500

    📦 NEW: laravel template

---
+++ template/laravel/tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+	purge: [
+		'./resources/**/*.blade.php',
+		'./resources/**/*.js',
+		'./resources/**/*.vue'
+	],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
+};
