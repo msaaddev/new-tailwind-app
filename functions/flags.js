a81cf0d0575b70f0770a96f8db7f688857b42bfe
msaaddev <mrsaadirfan@gmail.com>
2021-06-20 22:34:19 +0500

    📦 NEW: cli flags

---
+++ functions/flags.js
@@ -0,0 +1,17 @@
+module.exports = [
+	'--basic',
+	'-b',
+	'--next',
+	'--next.js',
+	'-n',
+	'--react',
+	'--react.js',
+	'-r',
+	'--gatsby',
+	'--gatsby.js',
+	'-g',
+	'--laravel',
+	'-l',
+	'--vue3',
+	'-v'
+];
