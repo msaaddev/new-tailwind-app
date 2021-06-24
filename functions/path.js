a944f85e779704a708a46d7057388db08e73ea91
msaaddev <mrsaadirfan@gmail.com>
2021-06-24 17:26:02 +0500

    🐛 FIX: gitignore path

--- functions/path.js
+++ functions/path.js
@@ -32,12 +32,12 @@ const basicTailwind = (name, currentDir) => {
 		src: `${currentDir}/template/basic/src`,
 		pkgJSON: `${currentDir}/template/basic/package.json`,
 		destPkgJSON: `${path}/package.json`,
-		gitIgnore: `${currentDir}/template/basic/.gitignore`,
+		gitIgnore: `${currentDir}/.gitignore`,
 		winDist: `${currentDir}${slash}template${slash}basic${slash}dist`,
 		winSrc: `${currentDir}${slash}template${slash}basic${slash}src`,
 		winPkgJSON: `${currentDir}${slash}template${slash}basic${slash}package.json`,
 		winDestPkgJSON: `${path}${slash}package.json`,
-		winGitIgnore: `${currentDir}${slash}template${slash}basic${slash}.gitignore`
+		winGitIgnore: `${currentDir}${slash}.gitignore`
 	};
 };
 
